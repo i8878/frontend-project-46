e1e9ea365ba8eab546006a46c77066b823b7e686
Иван Фроленков <work1113@mail.ru>
2026-08-04 23:29:45 +0300

    update Makefile

--- Makefile
+++ Makefile
@@ -4,5 +4,6 @@ publish:
 	npm publish --dry-run
 lint:
 	npx eslint .
- 
+test:
+	NODE_OPTIONS=--experimental-vm-modules npx jest 
 
--- __tests__/filereader.test.js
+++ __tests__/filereader.test.js
@@ -1,13 +1,18 @@
 import fileDiff from '../src/filereader.js'
 
-const path1
-const path2
+let path1
+let path2
 
 beforeAll(() => {
-    path1 = '../file1.json'
-    path2 = '../file2.json' 
+    path1 = './file1.json'
+    path2 = './file2.json' 
 })
 
 test('non difference', () => {
-    expect(fileDiff(path1, path1)).toEqual([ "host: hexlet.io", "timeout: 50", "proxy: 123.234.53.22", "follow: false" ])
+    expect(fileDiff(path1, path1)).toEqual([
+        { label: " ", property: "follow", value: false },
+        { label: " ", property: "host", value: "hexlet.io" }, 
+        { label: " ", property: "proxy", value: "123.234.53.22" }, 
+        { label: " ", property: "timeout", value: 50 } 
+    ])
 })
